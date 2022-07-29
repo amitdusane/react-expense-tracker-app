3c8ee3ca643e98a800593c4836b3e43aa951fb6d
amitd <amit.dusane@medly.com>
2022-07-29 17:50:41 +0530

    Basic state management for ExpenseItem title field

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,24 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
+import React, { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const changeTitle = () => {
+    setTitle("I'm clicked");
+    console.log("I'm clicked");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={changeTitle}>Change title</button>
     </Card>
   );
 }
